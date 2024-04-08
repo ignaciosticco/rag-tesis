3ca380599e040e2c12885145bc06f276f034ee44
ignaciosticco <ignaciosticco@gmail.com>
2024-04-08 17:20:38 -0300

    pruebo cargar la tesis

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,12 +18,12 @@ pregunta = st.text_input("Hacé tu pregunta", 'De que trata la tesis?')
 st.write('Hola')
 
 #### Importacion y chunkeo de la base de datos ####
-'''
+
 loader = TextLoader("tesis_doctoral.txt", encoding='utf-8')
 text_documents = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-documents = text_splitter.split_documents(text_documents)
-embeddings = OpenAIEmbeddings()
-vectorstore = DocArrayInMemorySearch.from_documents(documents, embeddings) # Embed the chunked documents
-'''
+#documents = text_splitter.split_documents(text_documents)
+#embeddings = OpenAIEmbeddings()
+#vectorstore = DocArrayInMemorySearch.from_documents(documents, embeddings) # Embed the chunked documents
+
 #st.write(st.secrets["things_i_like"])

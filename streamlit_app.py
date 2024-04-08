11fc5f787483fa173eb06e13003c7ab92888de03
ignaciosticco <ignaciosticco@gmail.com>
2024-04-08 16:13:10 -0300

    pongo las librerias a ver si se la banca

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+from langchain.prompts import ChatPromptTemplate
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import DocArrayInMemorySearch
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 
 st.write('Hello world!')
